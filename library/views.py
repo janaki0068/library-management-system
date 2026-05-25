b853cf251bf38817f9159af9e887fd7c72665c85
anushka155 <annggrg@gmail.com>
2026-05-25 15:14:27 +0545

    register/login

--- library/views.py
+++ library/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        User.objects.create_user(
+            username=username, 
+            email=email, 
+            password=password
+            )
+        return redirect('login')
     return render(request, 'register.html')
 
 def login(request):
